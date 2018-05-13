d03e2aac5e44f71474042ab5c336277b8eff243d
Rachit Belwariar <gbelwariar@gmail.com>
2018-05-13 09:47:10 +0530

    Update home-controller.js

--- Mining_Project/home/home-controller.js
+++ Mining_Project/home/home-controller.js
@@ -1,14 +1,34 @@
 function HomeCtrl($state) {		
 		this.$onInit = (function() {
 			this.matrix = {};
+			this.brrParameters = {};		// Contains 'p', 'Cm', 'r'.
+			this.bmcParameters = {};		// Contains 'Core'.
+			this.commonParameters = {};		// Contains 'v', 'ro'.
 			this.stateService_ = $state;
 		}).bind(this);
 }
 
 
 HomeCtrl.prototype.initiateMatrixGeneration = function() {
-	this.stateService_.go('matrix', {matrix: this.matrix});
+	let brr = computeBRR(this.brrParameters, this.commonParameters);
+	let bmc = computeBMC(this.bmcParameters, this.commonParameters);
+	this.stateService_.go('matrix', {
+		matrix: this.matrix,
+		brr: brr,
+		bmc: bmc
+	});
 };
+
+
+function computeBRR(brrParameters, commonParameters) {
+	return (brrParameters.p - brrParameters.Cm) *
+			brrParameters.r * commonParameters.v * commonParameters.ro;
+}
+
+
+function computeBMC(bmcParameters, commonParameters) {
+	return bmcParameters.Core * commonParameters.v * commonParameters.ro;
+}
 
 
 angular
